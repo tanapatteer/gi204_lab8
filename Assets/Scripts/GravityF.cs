d6188f7439a8ba40c2cdd9c0dcf7cf82d2a5e193
tanapatteer <tanapat.teer@bumail.net>
2025-03-13 16:08:05 +0700

    add script to project

--- Assets/Scripts/GravityF.cs
+++ Assets/Scripts/GravityF.cs
@@ -1,16 +1,53 @@
+using System;
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GravityF : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody rb;
+
+    const float G = 0.00667f;
+
+    public static List<GravityF> gravityObjectList;
+
+    private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+        if (gravityObjectList == null)
+        {
+            gravityObjectList = new List<GravityF>();
+            gravityObjectList.Add(this);
+        }
+        gravityObjectList = new List<GravityF>();
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var obj in gravityObjectList)
+        {
+            //call Attract
+            if (obj!=this)
+            {
+                 Attract(obj);     
+            }
+           
+        }
+       
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Attract(GravityF other)
     {
+        Rigidbody otherRb = other.rb;
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+        
+        float forceMagnitude = G * (rb.mass * otherRb.mass/ Mathf.Pow(distance, 2));
+        Vector3 gavityForce = forceMagnitude * direction.normalized;
+        
+        otherRb.AddForce(gavityForce);
+            
         
     }
 }
